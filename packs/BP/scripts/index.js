371d0aa15b829091d47906b0e068d32fa788d2ac
SmokeyStack <seansyahril04@gmail.com>
2022-10-16 22:53:55 -0400

    Tweaked init spawn

--- packs/BP/scripts/index.js
+++ packs/BP/scripts/index.js
@@ -1,14 +1,26 @@
-import { world } from "@minecraft/server";
+import { EntityQueryOptions, Location, world } from "@minecraft/server";
 
-let currentTick = 0;
 let hasRan = false;
+let query = {};
 
 world.events.tick.subscribe(async () => {
-    currentTick++;
-    if (!hasRan && (currentTick % 20 == 0)) {
+    if (!hasRan) {
         await world.getDimension("overworld").runCommandAsync("fill -3 63 -3 3 63 3 grass 0 replace air");
         await world.getDimension("overworld").runCommandAsync("setworldspawn 0 64 0");
         await world.getDimension("overworld").runCommandAsync("setblock 0 64 0 sapling");
         hasRan = true;
     }
 });
+
+function test() {
+    let players = world.getDimension("overworld").getPlayers(query);
+    for (let player of players) {
+        if (player.location.y <= -64) {
+            player.teleport(new Location(0, 64, 0), world.getDimension("overworld"), player.rotation.x, player.rotation.y);
+            world.events.tick.unsubscribe(test);
+        }
+    }
+
+}
+
+world.events.tick.subscribe(test);
