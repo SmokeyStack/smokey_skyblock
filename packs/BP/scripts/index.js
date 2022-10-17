5ddd57f9c6cf0389e023ae608fa382d1a6e67760
SmokeyStack <seansyahril04@gmail.com>
2022-10-17 07:01:59 -0400

    Tweaked script
    - Modified so that island script executes only once.
    - Renamed functions

--- packs/BP/scripts/index.js
+++ packs/BP/scripts/index.js
@@ -1,26 +1,26 @@
-import { EntityQueryOptions, Location, world } from "@minecraft/server";
+import { Location, world } from "@minecraft/server";
 
 let hasRan = false;
 let query = {};
 
 world.events.tick.subscribe(async () => {
-    if (!hasRan) {
+    if (!hasRan && (world.getAbsoluteTime() <= 2000)) {
         await world.getDimension("overworld").runCommandAsync("fill -3 63 -3 3 63 3 grass 0 replace air");
         await world.getDimension("overworld").runCommandAsync("setworldspawn 0 64 0");
-        await world.getDimension("overworld").runCommandAsync("setblock 0 64 0 sapling");
+        await world.getDimension("overworld").runCommandAsync("fill 0 64 0 0 64 0 sapling 0 replace air");
         hasRan = true;
     }
 });
 
-function test() {
+function escapeVoid() {
     let players = world.getDimension("overworld").getPlayers(query);
     for (let player of players) {
         if (player.location.y <= -64) {
             player.teleport(new Location(0, 64, 0), world.getDimension("overworld"), player.rotation.x, player.rotation.y);
-            world.events.tick.unsubscribe(test);
+            world.events.tick.unsubscribe(escapeVoid);
         }
     }
 
 }
 
-world.events.tick.subscribe(test);
+world.events.tick.subscribe(escapeVoid);
