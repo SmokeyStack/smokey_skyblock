7454c0c8ca89d3ab38d04e346b2d4cb1a1de6d52
SmokeyStack <seansyahril04@gmail.com>
2023-02-08 22:54:14 -0500

    World set up should be completed

--- packs/BP/scripts/index.js
+++ packs/BP/scripts/index.js
@@ -1,39 +1,38 @@
-import { BlockLocation, ItemStack, Location, MinecraftBlockTypes, MinecraftItemTypes, world } from "@minecraft/server";
+import { BlockLocation, ItemStack, MinecraftBlockTypes, MinecraftItemTypes, Vector, system, world } from "@minecraft/server";
 
 let query = {};
 
-async function setup() {
-    await world.getDimension("overworld").runCommandAsync("tickingarea add -16 63 -16 31 63 31 spawn true")
-        .then(world.getDimension("overworld").runCommandAsync("setworldspawn 0 64 0"))
-        .then(world.getDimension("overworld").runCommandAsync("tp @a 0 64 0"))
-        .then(world.getDimension("overworld").runCommandAsync("fill -3 63 -3 3 63 3 grass 0 replace air"))
-        .then(world.getDimension("overworld").runCommandAsync("fill 0 64 0 0 64 0 sapling 0 replace air"))
-        .then(world.getDimension("overworld").runCommandAsync("fill 2 63 0 2 63 0 crimson_nylium 0 replace grass"))
-        .then(world.getDimension("overworld").runCommandAsync("fill -2 63 0 -2 63 0 warped_nylium 0 replace grass"))
-        .then(world.events.tick.unsubscribe(setup));
-}
-
-world.events.tick.subscribe(setup);
-
-world.events.beforeItemUseOn.subscribe(async (eventData) => {
-    let block = world.getDimension("overworld").getBlock(eventData.blockLocation);
-    if (eventData.item.typeId == "minecraft:potion" && eventData.item.data == 3 && block.typeId == "minecraft:stone") {
-        eventData.cancel = true;
-        block.setType(MinecraftBlockTypes.deepslate);
-        await eventData.source.runCommandAsync("clear @s potion 3 1").then(eventData.source.runCommandAsync("give @s minecraft:glass_bottle"));
-    }
-});
-
-function escapeVoid() {
-    let players = world.getDimension("overworld").getPlayers(query);
+const setupId = system.runSchedule(() => {
+    world.getDimension('overworld').fillBlocks(new BlockLocation(-3, 63, -3), new BlockLocation(3, 63, 3), MinecraftBlockTypes.grass);
+    let players = world.getDimension('overworld').getPlayers();
 
     for (let player of players) {
-        if (player.location.y <= -64) {
-            player.teleport(new Location(0, 64, 0), world.getDimension("overworld"), player.rotation.x, player.rotation.y);
-            world.events.tick.unsubscribe(escapeVoid);
+        let tags = player.getTags()
+        if (!tags.includes('safe')) {
+            player.teleport(new Vector(0, 64, 0), world.getDimension("overworld"), 0, 0, false);
+            player.onScreenDisplay.setActionBar("Please wait for the set up system to finish.\nThe process takes 5 minutes upon creating\nthe world due to how slow world generation is");
+        }
+
+    }
+}, 20);
+
+system.runSchedule(() => {
+    system.clearRunSchedule(setupId);
+
+    world.getDimension('overworld').fillBlocks(new BlockLocation(0, 64, 0), new BlockLocation(0, 64, 0), MinecraftBlockTypes.sapling);
+    world.getDimension('overworld').fillBlocks(new BlockLocation(-2, 63, 0), new BlockLocation(-2, 63, 0), MinecraftBlockTypes.crimsonNylium);
+    world.getDimension('overworld').fillBlocks(new BlockLocation(2, 63, 0), new BlockLocation(2, 63, 0), MinecraftBlockTypes.warpedNylium);
+    world.getDimension("overworld").runCommandAsync("setworldspawn 0 64 0");
+
+    let players = world.getDimension('overworld').getPlayers();
+    for (let player of players) {
+        let tags = player.getTags()
+        if (!tags.includes('safe')) {
+            player.addTag('safe');
+            player.onScreenDisplay.setActionBar("You are now free to move");
         }
     }
-}
+}, 6000);
 
 function detectCoalToDiamond(eventData) {
     let coal_location = eventData.block.location.offset(0, -1, 0);
@@ -49,7 +48,7 @@ function detectCoalToDiamond(eventData) {
 function detectCoral(eventData) {
     if (eventData.currentTick % 200 == 0)
         for (let player of world.getDimension("overworld").getPlayers(query)) {
-            let blockLookedAt = player.getBlockFromViewVector({ maxDistance: 8 });
+            let blockLookedAt = player.getBlockFromViewDirection({ maxDistance: 8 });
 
             if (blockLookedAt.typeId == "minecraft:coral_fan_dead") {
                 if (blockLookedAt.isWaterlogged && blockLookedAt.permutation.getProperty("coral_color").value == "red" && (detectFlowingWater(blockLookedAt, 'n') || detectFlowingWater(blockLookedAt, 'e') || detectFlowingWater(blockLookedAt, 's') || detectFlowingWater(blockLookedAt, 'w')))
@@ -104,14 +103,16 @@ function dropEchoShard(eventData) {
         world.getDimension("overworld").spawnItem(new ItemStack(MinecraftItemTypes.echoShard), new BlockLocation(Math.floor(eventData.hurtEntity.location.x), Math.floor(eventData.hurtEntity.location.y + 1), Math.floor(eventData.hurtEntity.location.z)));
 }
 
+world.events.beforeItemUseOn.subscribe(async (eventData) => {
+    let block = world.getDimension("overworld").getBlock(eventData.blockLocation);
+    if (eventData.item.typeId == "minecraft:potion" && eventData.item.data == 3 && block.typeId == "minecraft:stone") {
+        eventData.cancel = true;
+        block.setType(MinecraftBlockTypes.deepslate);
+        await eventData.source.runCommandAsync("clear @s potion 3 1").then(eventData.source.runCommandAsync("give @s minecraft:glass_bottle"));
+    }
+});
+
 world.events.beforePistonActivate.subscribe(detectCoalToDiamond);
-
-world.events.entityCreate.subscribe(detectVineToLichen);
-
-world.events.entityHurt.subscribe(dropEchoShard);
-
-world.events.tick.subscribe(detectCoral);
-world.events.tick.subscribe(escapeVoid);
 
 world.events.blockPlace.subscribe(async (eventData) => {
     if (eventData.block.typeId == "minecraft:sculk_shrieker" && world.getDimension("overworld").getBlock(eventData.block.location.offset(0, -1, 0)).typeId == "minecraft:soul_sand") {
@@ -119,3 +120,9 @@ world.events.blockPlace.subscribe(async (eventData) => {
         world.getDimension("overworld").runCommandAsync(`setblock ${eventData.block.location.x} ${eventData.block.location.y} ${eventData.block.location.z} sculk_shrieker [\"can_summon\": true]`);
     }
 });
+
+world.events.entitySpawn.subscribe(detectVineToLichen);
+
+world.events.entityHurt.subscribe(dropEchoShard);
+
+world.events.tick.subscribe(detectCoral);
